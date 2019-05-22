a135d93e12d4e4cfa2308e1c93227a1bb938d8f7
Alexander <alex_1128@hotmail.es>
2019-05-22 06:19:23 -0500

    Alert and avatar

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -1,14 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class AlertPage extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Alert page"),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Mostrar alerta"),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _mostrarAlerta(context),
+        ),
+      ),
     );
+  }
+
+  void _mostrarAlerta(BuildContext context) {
+
+    showDialog(
+
+      context: context,
+      barrierDismissible: true,
+      builder: (context){
+        return AlertDialog(
+          // title: Text("Titulo"),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.5)),
+          backgroundColor: Colors.black,
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              FlutterLogo(size: 100.0,),
+              SizedBox(height: 15.0,),
+              Text('Gracias por su visita, vuelva pronto', style: TextStyle(color: Colors.white),),
+              
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("Cancelar"),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              child: Text("Aceptar"),
+              onPressed: (){
+
+                Navigator.of(context).pop();
+                _otherAlert(context);
+              },
+            )
+          ],
+        );
+      }
+
+    );
+  }
+
+  void _otherAlert(BuildContext context){
+
+    showDialog(
+      
+      context: context,
+      builder: (context){
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.5)),
+          backgroundColor: Colors.black,
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              FlutterLogo(size: 100.0),
+              SizedBox(height: 15.5),
+              Text("Gracias", style: TextStyle(color: Colors.white),),
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("Cerrar"),
+              onPressed: () => Navigator.of(context).pop(),
+            )
+          ],
+        );
+      }
+    );
+
   }
 }
--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -8,6 +8,32 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Avatar page"),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://www.eldiario.es/fotos/Stan-Lee-lanzando-presentacion-Spiderman_EDIIMA20181112_0851_19.jpg'),
+              radius: 23.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text("SL"),
+              backgroundColor: Colors.purple,
+            ),
+          )
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        child: Center(
+          child: FadeInImage(
+            image: NetworkImage("https://www.infobae.com/new-resizer/T4gHub2jEqfFKWCWbYp784k8F8w=/1200x0/filters:quality(100)/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2018/01/09205640/stan-lee-2.png"),
+            placeholder: AssetImage("assets/image/jar-loading.gif"),
+            fadeInDuration: Duration(milliseconds: 250),
+          ),
+        ),
       ),
     );
   }

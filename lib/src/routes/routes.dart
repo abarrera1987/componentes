d7f19d69b34b6d28d596a4eabfcb3e2e40501159
kubo mac 8 <kubomac8@iMac-de-kubo.local>
2019-05-22 10:59:22 -0500

    Animator Container and Inputs

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -3,6 +3,8 @@ import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
+import 'package:componentes/src/pages/animated_container.dart';
+import 'package:componentes/src/pages/input_page.dart';
 
 Map<String, WidgetBuilder> getAplicationRoutes() {
   return <String, WidgetBuilder>{
@@ -10,5 +12,7 @@ Map<String, WidgetBuilder> getAplicationRoutes() {
     'alert': (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
     'card': (BuildContext context) => CardPage(),
+    'animatedContainer': (BuildContext context) => AnimaterContainerPage(),
+    'inputs': (BuildContext context) => InputPage(),
   };
 }
